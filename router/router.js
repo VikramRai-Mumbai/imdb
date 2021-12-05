1a43b7d9565099530b1aed8b66c6a356c6627e9b
Vikram Kumar <vikramraimumbai@gmail.com>
2021-12-05 11:56:44 +0530

    changes

--- router/router.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// simple route
-router.get("/", (req, res) => {
-  res.json({ message: "Welcome to router." });
-});
-
-module.exports = router;
